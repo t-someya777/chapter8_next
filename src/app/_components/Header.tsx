ca9863f632a28b2c3c39cb6dd2d281ed6aea4b47
t_someya <t_someya@wan55.co.jp>
2025-08-02 13:11:02 +0900

    ヘッダー修正

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,11 +1,53 @@
+
+// export default function Header() {
+//   return (
+//     <header className={styles.header}>
+//       <Link className={styles.headerItem} href='/'>Blog</Link>
+//       <Link className={styles.headerItem} href='/contact'>お問い合わせ</Link>
+//     </header>
+//   )
+// }
+
+
+'use client'
+
 import Link from "next/link"
 import styles from "./Header.module.scss"
+import { useSupabaseSession } from "../_hooks/useSupabaseSession"
+import { supabase } from "@/utils/supabase"
 
-export default function Header() {
+export const Header = () => {
+  const handleLogout = async() => {
+    await supabase.auth.signOut()
+    window.location.href = '/'
+  }
+
+  const {session, isLoading} = useSupabaseSession()
+
   return (
     <header className={styles.header}>
       <Link className={styles.headerItem} href='/'>Blog</Link>
-      <Link className={styles.headerItem} href='/contact'>お問い合わせ</Link>
-    </header>
+    {!isLoading && (
+      <div>
+        {session ? (
+          <>
+            <Link href="/admin" className={styles.headerItem}>
+              管理画面
+            </Link>
+            <button onClick={handleLogout} className={styles.headerItem}>ログアウト</button>
+          </>
+        ) : (
+          <>
+            <Link className={styles.headerItem} href='/contact'>
+              お問い合わせ
+            </Link>
+            <Link href="/login" className={styles.headerItem}>
+              ログイン
+            </Link>
+          </>
+        )}
+        </div>
+      )}
+      </header>
   )
 }
